f04c6817585508d3589088bf47ce019d64946aaf
Matiako <32797957+Matiako@users.noreply.github.com>
2020-06-05 09:25:53 +0200

    pierwszy

---
+++ src/test/java/StringHelperTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringHelperTest {
+
+
+    @Test
+    public void test(){
+    StringHelper helper = new StringHelper();
+        assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+}
+}
