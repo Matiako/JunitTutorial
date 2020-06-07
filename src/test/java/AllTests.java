6f593064e03421cf4d1d5462a88bba0b0bd32d0d
BYDALS <au6stein>
2020-06-07 23:00:44 +0200

    2

---
+++ src/test/java/AllTests.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({QuickBeforeAfterTest.class,StringHelperTest.class})
+public class AllTests {
+
+}
